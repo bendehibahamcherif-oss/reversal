d64e8f6e6344685d6a021e451a6debe96f1d1e92
bendehibahamcherif-oss <bendehiba.hamcherif@gmail.com>
2026-05-23 17:31:15 +0400

    Add user persistence model

---
+++ models/User.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+
+    role: {
+      type: String,
+      default: 'user',
+    },
+
+    watchlists: [
+      {
+        name: String,
+        symbols: [String],
+      },
+    ],
+
+    workspaceLayouts: {
+      type: Object,
+      default: {},
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model(
+  'User',
+  UserSchema
+);
